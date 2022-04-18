7050d3665de56940aa005860296d62495710be8b
Mel <meljohnpualon@gmail.com>
2022-04-18 09:37:10 +0800

    added getpost composables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,49 +3,30 @@
         <h1>Hey there</h1>
         <div v-if="error">{{ error }}</div>
         <div v-if="posts.length">
-            <PostList v-if="showPost" :posts="posts" />
+            <PostList :posts="posts" />
         </div>
         <div v-else>
             Loading...
         </div>
         
-        <button @click="showPost = !showPost">Toggle Post</button>
+        
         <button @click="posts.pop()">Delete Post</button>
     </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
 import PostList from '../components/PostList.vue'
+import getPost from '../composables/getPost'
 
 export default {
     name: 'home',
     components: { PostList },
     setup() {
-        const posts = ref([])
-        const error = ref(null)
-
-        const load = async () => {
-            try {
-                let data = await fetch('http://localhost:3000/posts')
-                // console.log(data)
-                if(!data.ok) {
-                    throw new Error('No data Available')
-                }
-
-                posts.value = await data.json()
-            }
-            catch(err) {
-                error.value = err.message
-                console.log(error.value)
-            }
-        }
+        const { posts, error, load } = getPost();
 
         load()
-
-        const showPost = ref(true)
-
-        return { posts, showPost, load, error }
+        
+        return { posts, error, load }
     }
 }
 </script>
